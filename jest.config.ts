76fec5b64adff4e174930ab5e97003f3e6213bf1
David Alberca Rodriguez <dar44@alu.ua.es>
2025-11-12 19:53:16 +0100

    configuracion Jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }] as [string, unknown],
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+};
+
+export default createJestConfig(customJestConfig);
