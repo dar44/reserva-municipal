88236139bb35fe048185fd89172ec79f0c636a48
David Alberca Rodriguez <dar44@alu.ua.es>
2025-12-04 15:36:36 +0100

    Plazas ahora son plazas disponibles

--- src/app/worker/cursos/page.tsx
+++ src/app/worker/cursos/page.tsx
@@ -43,7 +43,7 @@ export default async function WorkerCursosPage() {
             <th className="px-4 py-2 text-left">Nombre</th>
             <th className="px-4 py-2 text-left">Descripción</th>
             <th className="px-4 py-2 text-left">Fecha</th>
-            <th className="px-4 py-2 text-left">Plazas</th>
+            <th className="px-4 py-2 text-left">Plazas disponibles</th>
             <th className="px-4 py-2 text-left">Estado</th>
             <th className="px-4 py-2 text-left">Acciones</th>
           </tr>
